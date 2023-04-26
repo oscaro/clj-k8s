553740bfa9b12dd7b8d2d3ad7adb0c8a92d8ed63
iomonad <iomonad@riseup.net>
2023-04-26 14:24:33 +0200

    auth: make the auth function more generic

--- src/clj_k8s/api.clj
+++ src/clj_k8s/api.clj
@@ -2,7 +2,10 @@
   (:require [yaml.core :as yaml]
             [schema.core :as s]
             [clj-k8s.gke :as gke]
-            [clj-k8s.utils :refer [find-named]]))
+            [clj-k8s.utils :refer [find-named]]
+            [clj-k8s.models :refer :all]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 ;;; =====================================
 ;;;  Public API
@@ -10,44 +13,94 @@
 
 ;;; Constants
 
-(defonce default-ns "default")
+(defonce ^:private default-ns "default")
 
 
 ;;; For application running in Kubernetes context only
-(defonce default-k8s-svc-host (System/getenv "KUBERNETES_SERVICE_HOST"))
-(defonce default-k8s-svc-port (System/getenv "KUBERNETES_SERVICE_PORT"))
+(defonce ^:private default-k8s-svc-host (System/getenv "KUBERNETES_SERVICE_HOST"))
+(defonce ^:private default-k8s-svc-port (System/getenv "KUBERNETES_SERVICE_PORT"))
 
 
 ;;; See: https://cloud.google.com/docs/authentication/application-default-credentials?hl=fr
-(defonce default-cluster-service-account-dir
+(defonce ^:private default-cluster-service-account-dir
   (or (System/getenv "GOOGLE_APPLICATION_CREDENTIALS")
       "/var/run/secrets/kubernetes.io/serviceaccount"))
 
 ;;; Kubernetes Token
-(defonce token-from-env (str (System/getenv "K8S_TOKEN")))
+(defonce ^:private token-from-env (str (System/getenv "K8S_TOKEN")))
 
 ;;; Honor `KUBECONFIG` before generating fallback one from
 ;;; the user `HOME`
-(defonce default-kube-config-path
+(defonce ^:private default-kube-config-path
   (or (System/getenv "KUBECONFIG")
       (str (System/getenv "HOME") "/.kube/config")))
+
+(def running-from-kube?
+  "Detect if the application is currently running on k8s
+   platform, or not"
+  (and (.exists (io/file "/var/run/secrets/kubernetes.io/serviceaccount"))
+       (every? some? [default-k8s-svc-host default-k8s-svc-port])))
+
+(defn- is-current-gke?
+  "Detect if the current context is a GKE cluster.
+   Logic is based on `gke_*` naming scheme"
+  [current-context]
+  (str/starts-with? current-context "gke_"))
 
 ;;; Authentification
 
 (defn mk-client
-  ""
+  "Generic client builder
+
+   There is several ways to instantiate the client:
+
+   1. Without any arguments, the function will fetch several env variables
+      to build the client. This method is useful for applications that run directly
+      in a Kubernetes Pod, or for executions context where env variables are used for
+      configurations, required env variables are:
+
+      - `KUBERNETES_SERVICE_HOST` - the Kubernetes Service Endpoint
+      - `KUBERNETES_SERVICE_HOST` - the Kubernetes Service Endpoint Port
+      - `KUBERNETES_TOKEN`        - the actual Kubernetes token to be used
+         See: https://docs.selectel.com/cloud/managed-kubernetes/instructions/service-account-token
+
+   2. The more straightforward, and easy to setup is from a map spec directly, every
+      elements are explicitly used for client building (probably this will be fetched
+      from your component management system like Integrant.
+
+   3. The last one is from, configuration file directly. The builder will auto-detect
+      if the current context is a GKE cluster. If so, you'll need to be sure that the
+      `GOOGLE_APPLICATION_CREDENTIALS` variable is set and corretly pointing to your SA
+      for fetching token from API.
+
+      WARN: If not running on GKE, you'll need to explicitly setup your token variable to use
+      with your current context.
+
+      Note: This methods is used by default if the builder isn't call with arguments
+            AND all variables are not satisfied."
   {:added "1.25.8.2"}
-  ([] (mk-client default-kube-config-path token-from-env))
-  ([kube-config] (mk-client kube-config token-from-env))
+  ([]
+   (if (and default-k8s-svc-host default-k8s-svc-port (not-empty token-from-env))
+     (s/validate KubeClient
+                 {:base-url default-k8s-svc-host
+                  :auths     {"BearerToken" (str "Bearer " token-from-env)}
+                  :namespace default-ns})
+     (mk-client default-kube-config-path)))
+  ([kube-config]
+   (if (map? kube-config)
+     (let [{:keys [base-url token namespace]} (s/validate KubeClientSpec kube-config)]
+       (s/validate KubeClient {:base-url base-url
+                               :auths     {"BearerToken" (str "Bearer " token)}
+                               :namespace (or namespace default-ns)}))
+     (mk-client kube-config token-from-env)))
   ([kube-config token]
    (let [{:keys [clusters contexts current-context] :as conf}
          (yaml/from-file kube-config)
          context (find-named current-context contexts)
          cluster (find-named (get-in context [:context :cluster]) clusters)
-         ;; TODO: Detect if current context is from GKE or not
-         token (if false
-                 (or token token-from-env)
-                 (gke/get-google-access-token))]
+         token (if (is-current-gke? current-context)
+                 (gke/get-google-access-token)
+                 token)]
      {:base-url  (get-in cluster [:cluster :server])
       :auths     {"BearerToken" (str "Bearer " token)}
       :namespace (get-in context [:context :namespace] default-ns)})))
